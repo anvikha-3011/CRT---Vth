3526e19241fff13fb5d636e5b1a099010a327c45
Anvikha <anvikhasalivendra@gmail.com>
2026-09-21 09:49:08 +0530

    Add Bank class with methods for initializing balance and crediting amount

---
+++ Practice/M03_Object-Oriented_Programming_Concepts/S02/Encapsulation.py
@@ -0,0 +1,6 @@
+class Bank:
+    def_init_(self,balance):
+        self.__balance = balance  # Private attribute   
+    def_credit(self, amount):
+        self.__balance += amount  # Method to credit amount to balance
+    
