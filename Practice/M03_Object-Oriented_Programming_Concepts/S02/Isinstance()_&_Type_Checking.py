cbcdaa397b59fef0eb5e66ff51e8e610075f7cde
Anvikha <anvikhasalivendra@gmail.com>
2026-09-22 13:33:26 +0530

    Add new files for type checking and single linked list implementations

---
+++ Practice/M03_Object-Oriented_Programming_Concepts/S02/Isinstance()_&_Type_Checking.py
@@ -0,0 +1,20 @@
+#type()
+
+a = 10 
+b = 5.6 
+c = "Hello"
+d = [1, 2, 3, 4, 5, 4]
+e = (1, 2, 3, 4, 5)
+f = {1, 2, 3, 4, 5}
+g = {"name": "Anvikha"}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
+'''
+isinstance(): 
+'''
